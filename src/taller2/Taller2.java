5a5723d2da65ac76d105e44fbd8a3efb98997f81
Bryan Campos <b.camposvindas@hotmail.com>
2017-08-07 09:15:24 -0600

    Menu y Adivinar completado

---
+++ src/taller2/Taller2.java
@@ -0,0 +1,11 @@
+package taller2;
+
+public class Taller2 {
+
+    
+    public static void main(String[] args) {
+        Menu ventana = new Menu();
+        ventana.setVisible(true);
+    }
+    
+}
